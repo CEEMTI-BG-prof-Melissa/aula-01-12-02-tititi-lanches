6e06da4efb158006aeb42704b593b353e627ae0e
Otávio Faria <otavio.fbreis@aluno.edu.es.gov.br>
2025-09-03 10:41:23 -0300

    Create index.php

---
+++ aula01 3T/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+            echo "Bia e Otávio"
+        ?>
+    </body>
+</html>
