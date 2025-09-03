913c1fa0435840ea1620632986258074939ab044
Otávio Faria <otavio.fbreis@aluno.edu.es.gov.br>
2025-09-03 11:02:36 -0300

    Create index.php

---
+++ Aula02- 3T/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php
+            $nome1 = "Bia";
+            $nome2 = "Otávio";
+
+            echo "meu nome é " .$nome1 "e minha dupla é " .$nome2
+          ?>
+    </body>
+</html>
